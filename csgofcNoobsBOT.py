bd34d8d9eccf13080192e4ef716006ddcf226d1a
Alejandro <alerodri1994@gmail.com>
2017-11-12 11:35:57 +0100

    Formateo de codigo

--- csgofcNoobsBOT.py
+++ csgofcNoobsBOT.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
-import telebot
 import sys
 from imp import reload
+
+import telebot
 
 ############################################
 #                 DATOS                    #
 ############################################
 reload(sys)
- 
+
 TOKEN = '309560265:AAHBh78UeZkJPwUudgnC0KIunq3lp8Z8XZI'
- 
+
 bot = telebot.TeleBot(TOKEN)
- 
+
+
 ############################################
 #                 FUNCIONES                #
 ############################################
- 
+
 
 def command_info(m):
     cid = m.chat.id
     infile = open('util/info.txt', 'r', encoding='utf-8')
     mensaje = infile.read()
     infile.close()
-    bot.send_message( cid, str(mensaje), parse_mode='Markdown')
-    print ("Enviando info...")
+    bot.send_message(cid, str(mensaje), parse_mode='Markdown')
+    print("Enviando info...")
 
- 
+
 ############################################
 #                 LISTENER                 #
 ############################################
- 
+
 def listener(messages):
     for m in messages:
         cid = m.chat.id
@@ -41,31 +43,34 @@ def listener(messages):
             f = open('log.txt', 'a')
             f.write(mensaje + "\n")
             f.close()
-            print (mensaje)
+            print(mensaje)
         elif m.content_type == 'new_chat_participant':
-            bot.send_message( cid, 'Bienvenido '+ str(m.chat.first_name))
+            bot.send_message(cid, 'Bienvenido ' + str(m.chat.first_name))
             command_info(m)
-        
+
 
 bot.set_update_listener(listener)
- 
+
+
 ############################################
 #                 COMANDOS                 #
 ############################################
- 
+
 @bot.message_handler(commands=['start'])
 def command_start(m):
     comando = m.text[7:]
     if comando == 'info':
         command_info(m)
-    
+
+
 @bot.message_handler(commands=['info'])
 def command_z1(m):
-    command_info(m) 
+    command_info(m)
+
 
 ############################################
 #                 POLLING                  #
 ############################################
- 
+
 bot.skip_pending = True
 bot.polling(none_stop=True)
