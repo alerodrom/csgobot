768471d5de2f98ea74f2c24006bf988833bb9e86
Alejandro <alerodri1994@gmail.com>
2018-01-21 17:44:07 +0100

    Añadida bienvenidad personalizada y actualizado texto de bienvenida

--- csgofcNoobsBOT.py
+++ csgofcNoobsBOT.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 import sys
-from imp import reload
+from importlib import reload
 
 import telebot
 
@@ -19,12 +18,34 @@ bot = telebot.TeleBot(TOKEN)
 ############################################
 
 
-def command_info(m):
-    cid = m.chat.id
-    infile = open('util/info.txt', 'r', encoding='utf-8')
-    mensaje = infile.read()
-    infile.close()
-    bot.send_message(cid, str(mensaje), parse_mode='Markdown')
+def get_info(message):
+    chat_id = message.chat.id
+    file = open('util/info.txt', 'r', encoding='utf-8')
+    text = file.read()
+    file.close()
+    bot.send_message(chat_id, str(text), parse_mode='Markdown')
+
+
+def echo(message):
+    chat_id = message.chat.id
+    text = "Hola " + message.from_user.first_name + " has puesto: " + message.text[6:]
+    bot.send_message(chat_id, str(text), parse_mode='Markdown')
+
+
+def test(message):
+    """
+    file = open('util/info_compare.txt', 'r', encoding='utf-8')
+    text = file.read()
+    file.close()
+    alias = " (@" + message.from_user.username + "). " if message.from_user.username is not None else ". "
+    welcome = 'Bienvenido *' + str(message.from_user.first_name) + "*" + str(alias)
+    info = "Aqui tienes toda la información..."
+    if bot.get_chat(message.chat.id).pinned_message.text[:17] == text[:17]:
+        bot.reply_to(bot.get_chat(message.chat.id).pinned_message, welcome + info, parse_mode='Markdown')
+    else:
+        bot.send_message(message.chat.id, welcome, parse_mode='Markdown')
+        get_info(message)
+    """
 
 
 ############################################
@@ -32,19 +53,25 @@ def command_info(m):
 ############################################
 
 def listener(messages):
-    for m in messages:
-        cid = m.chat.id
-        if m.content_type == 'text':
+    for message in messages:
+        cid = message.chat.id
+        if message.content_type == 'text':
             if cid > 0:
-                mensaje = str(m.chat.first_name) + " [" + str(cid) + "]: " + m.text
+                mensaje = str(message.chat.first_name) + " [" + str(cid) + "]: " + message.text
             else:
-                mensaje = str(m.from_user.first_name) + "[" + str(cid) + "]: " + m.text
+                mensaje = str(message.from_user.first_name) + "[" + str(cid) + "]: " + message.text
             f = open('log.txt', 'a')
             f.write(mensaje + "\n")
             f.close()
-        elif m.content_type == 'new_chat_participant':
-            bot.send_message(cid, 'Bienvenido ' + str(m.chat.first_name))
-            command_info(m)
+        elif message.content_type == 'new_chat_members':
+            alias = " (@" + message.from_user.username + "). " if message.from_user.username is not None else ". "
+            welcome = 'Bienvenido *' + str(message.from_user.first_name) + "*" + str(alias)
+            info = "Aqui tienes toda la información..."
+            if bot.get_chat(message.chat.id).pinned_message.text[:17] == '🎮 CS:GO FC NOOBS':
+                bot.reply_to(bot.get_chat(message.chat.id).pinned_message, welcome + info, parse_mode='Markdown')
+            else:
+                bot.send_message(message.chat.id, welcome, parse_mode='Markdown')
+                get_info(message)
 
 
 bot.set_update_listener(listener)
@@ -58,12 +85,22 @@ bot.set_update_listener(listener)
 def command_start(m):
     comando = m.text[7:]
     if comando == 'info':
-        command_info(m)
+        get_info(m)
 
 
 @bot.message_handler(commands=['info'])
 def command_z1(m):
-    command_info(m)
+    get_info(m)
+
+
+@bot.message_handler(commands=['echo'])
+def command_echo(m):
+    echo(m)
+
+
+@bot.message_handler(commands=['test'])
+def command_reply_to_pinned(m):
+    test(m)
 
 
 ############################################
