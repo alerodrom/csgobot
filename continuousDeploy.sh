558d2ee70487a74d08102b72bc051a3a9c297af4
Alejandro <alerodri1994@gmail.com>
2018-01-21 18:16:09 +0100

    Actualizando fichero de configuración de docker

--- continuousDeploy.sh
+++ continuousDeploy.sh
@@ -6,3 +6,7 @@ docker run -d --name csgobot-python \
     --restart=always \
 	--expose=5000 \
     gurken2108/python3-java
+    bash -c "
+    echo 'Instalando requirements' && \
+    pip install -r requirements.txt && \
+    bash runBOT.sh"
