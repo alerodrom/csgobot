8392cbae343b41228d2e929085ebe92c045f4d30
Alejandro <alerodri1994@gmail.com>
2018-05-29 11:02:45 +0200

    New admins. Remove ci file.

--- continuousDeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d --name csgobot-python \
-	-v "/home/pomosoft-user/csgobot":/myapp \
- 	-w /myapp \
-    --restart=always \
-	--expose=5000 \
-    gurken2108/python3-java \
-    bash -c "echo 'Europe/Madrid' > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata && pip install -r requirements.txt && python3 csgo_bot.py"
